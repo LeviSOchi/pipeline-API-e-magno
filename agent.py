e39adce0f057dedc1a50b7370739e86bc9c69bd7
Levi Ochi <leviochi12@gmail.com>
2025-05-14 20:44:45 -0300

    Update agent

---
+++ agent.py
@@ -0,0 +1,31 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.postgres import PostgresTools
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Initialize PostgresTools with connection details
+postgres_tools = PostgresTools(
+    host="dpg-d0hvc36mcj7s739i1c40-a.oregon-postgres.render.com",
+    port=5432,
+    db_name="dbname_kult",
+    user="dbname_kult_user",
+    password="m1D7aWsbAD8IUoyXXp8RtV4i60Oh8Hsf",
+    table_schema="public",
+)
+
+# Create an agent with the PostgresTools
+agent = Agent(tools=[postgres_tools],
+              model=Groq(id="llama-3.3-70b-versatile"))
+
+agent.print_response("Fale todas as tabelas do banco de dados", markdown=True)
+
+agent.print_response("""
+Faça uma query para pegar todas as cotações de bitcoin na tabela bitcoin_dados
+""")
+
+agent.print_response("""
+Faça uma análise super complexa sobre o bitcoin usando os dados da tabela bitcoin_dados
+""")
