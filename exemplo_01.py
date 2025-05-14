6810afa2890d85176ecbfaf8a451c1789de7e854
Levi Ochi <leviochi12@gmail.com>
2025-05-13 21:30:22 -0300

    update da extração e transformação

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,7 +1,22 @@
 import requests
 
-url = "https://api.coinbase.com/v2/prices/spot"
+def extrair():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()
 
-response = requests.get(url)
+def transformar(dados_json):
+    valor = dados_json["data"]["amount"]
+    criptomoeda = dados_json["data"]["base"]
+    moeda = dados_json["data"]["currency"]
+    dados_tratados = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda
+                }
+    return dados_tratados
 
-print(response.json())
+if __name__ == "__main__":
+    dados_json = extrair()
+    dados_tratados = transformar(dados_json)
+    print(dados_tratados)
